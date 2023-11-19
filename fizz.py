0be284298c48082cf6c3c6f622f917e3f0f7f6cf
Yokoyoko <yudai20022002@icloud.com>
2023-11-19 17:49:04 +0900

    fizzの修正をした

--- fizz.py
+++ fizz.py
@@ -3,12 +3,12 @@ for i in range(100):
     num.append(i)
 
 for i in num:
-    if num%100==0:
+    if i%100==0:
         print('100の倍数')
     
-    if num%5==0:
+    if i%5==0:
         print('5の倍数である')
-    if num%10==0:
+    if i%10==0:
         print('10の倍数')
         
         
